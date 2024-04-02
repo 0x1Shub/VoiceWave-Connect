faa6aa3f54d63ecb7d4788a2b3d17f5012e21247
0x1Shub <shubhamyeram13@gmail.com>
2024-04-02 12:03:20 +0530

    Created: Route Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,15 @@ import Navbar from './components/Navbar.jsx';
 import Home from './pages/Home.jsx';
 
 import Authentication from './pages/Authentication.jsx';
+import Activate from './pages/Activate.jsx';
+import Rooms from './pages/Rooms.jsx';
+
 import './styles/app.scss';
 
-const  isAuthenticated  = false;
+const  isAuthenticated  = true;
+const user = {
+  activated: true,
+}
 
 
 function App() {
@@ -16,6 +22,8 @@ function App() {
       <Routes>
         <Route path='/' exact element={<GuestRoute element={<Home />} />} />
         <Route path="/authenticate" element={<GuestRoute element={<Authentication />}></GuestRoute>} />
+        <Route path='/activate' element={<SemiProtectedRoute element={<Activate />} />} />
+        <Route path='/rooms' element={<ProtectedRoute element={<Rooms />} />} />
       </Routes>
     </BrowserRouter>
   )
@@ -33,6 +41,27 @@ const GuestRoute = ({element}) => {
               element
           )
   );
+}
+
+
+const SemiProtectedRoute = ({ element }) => {
+  return !isAuthenticated ? (
+      <Navigate to="/" replace />
+  ) : isAuthenticated && !user.activated ? (
+      element
+  ) : (
+      <Navigate to="/rooms" replace />
+  );
+};
+
+const ProtectedRoute = ({element}) => {
+  return !isAuthenticated ? (
+    <Navigate to={'/'} replace />
+  ) : isAuthenticated && !user.activated ? (
+    <Navigate to={'/activate'} replace />
+  ) : (
+    element
+  )
 }
 
 export default App
