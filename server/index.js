fd4e75ad2ed2221fa4c8f5a37845a22106485e4a
0x1Shub <shubhamyeram13@gmail.com>
2024-08-09 18:58:31 +0530

    Created: Socket Server

--- server/index.js
+++ server/index.js
@@ -4,8 +4,17 @@ const router = require("./routes.js");
 const DbConnect = require("./database.js");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
+const ACTIONS = require("./actions.js");
 const app = express();
+
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(cookieParser());
 
@@ -34,6 +43,34 @@ app.get("/", (req, res) => {
   res.send("Hello from express");
 });
 
-app.listen(PORT, (req, res) => {
+// Sockets
+const socketUserMapping = {};
+
+io.on("connection", (socket) => {
+  console.log("new connection", socket.id);
+
+  socket.on(ACTIONS.JOIN, ({ roomId, user }) => {
+    socketUserMapping[socket.id] = user;
+
+    // new Map
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+
+    clients.forEach((clientId) => {
+      io.to(clientId).emit(ACTIONS.ADD_PEER, {
+        peerId: clientId,
+        createOffer: false,
+        user,
+      });
+      socket.emit(ACTIONS.ADD_PEER, {
+        peerId: clientId,
+        createOffer: true,
+        user: socketUserMapping[clientId],
+      });
+    });
+    socket.join(roomId);
+  });
+});
+
+server.listen(PORT, (req, res) => {
   console.log(`Server is listening on port ${PORT}`);
 });
